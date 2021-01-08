2fa579e34465f7e6e31757434fc06618c263641f
Gabriel <gabrielldarts@gmail.com>
2021-01-07 22:08:30 -0300

    read playlist user and your tracks

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,4 @@
-export function API_AUTHORIZE_SPOTIFY() {
+export function REQUEST_CODE_AUTHORIZATION() {
   const client_id = '01f9c1ff73124a728402ba1a817c489f';
   const redirect_uri = 'http://localhost:3000/';
   const scopes = 'user-read-private%20user-read-email%20playlist-modify-public';
@@ -29,6 +29,75 @@ export function REQUEST_TOKEN(code) {
       method: 'POST',
       headers: myHeaders,
       body: urlencoded,
+      redirect: 'follow',
+    },
+  };
+}
+
+export function REFRESH_TOKEN(refreshToken) {
+  let myHeaders = new Headers();
+  myHeaders.append(
+    'Authorization',
+    'Basic MDFmOWMxZmY3MzEyNGE3Mjg0MDJiYTFhODE3YzQ4OWY6MWExOTgwYmIwNTY2NGY1Njg5MjQyMjZlODIwMGI4NjQ=',
+  );
+  myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
+  myHeaders.append(
+    'Cookie',
+    '__Host-device_id=AQB7PIx_RKbN48G1GturYVuWLxLHoMJAXZYmwFBxdx6qMvoC0CoeRWm7Fe_xqxO28c7I2oRq9Ns28JwryPXjxNe_9YzBJl9jA7I',
+  );
+
+  let urlencoded = new URLSearchParams();
+  urlencoded.append('grant_type', 'refresh_token');
+  urlencoded.append('refresh_token', `${refreshToken}`);
+
+  return {
+    url: 'https://accounts.spotify.com/api/token',
+    options: {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow',
+    },
+  };
+}
+
+export function REQUEST_USER_PROFILE(token) {
+  let myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
+
+  return {
+    url: 'https://api.spotify.com/v1/me',
+    options: {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+    },
+  };
+}
+
+export function REQUEST_PLAYLISTS(token) {
+  let myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
+
+  return {
+    url: 'https://api.spotify.com/v1/me/playlists',
+    options: {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+    },
+  };
+}
+
+export function REQUEST_TRACKS_OF_PLAYLIST(token, url) {
+  let myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
+
+  return {
+    url: `${url}`,
+    options: {
+      method: 'GET',
+      headers: myHeaders,
       redirect: 'follow',
     },
   };
--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
-import { REQUEST_TOKEN, API_AUTHORIZE_SPOTIFY } from './Api.js';
+import React, { useEffect, useState } from 'react';
+import {
+  REQUEST_CODE_AUTHORIZATION,
+  REQUEST_TOKEN,
+  REFRESH_TOKEN,
+  REQUEST_USER_PROFILE,
+  REQUEST_PLAYLISTS,
+  REQUEST_TRACKS_OF_PLAYLIST,
+} from './Api.js';
 import useFetch from './Hooks/useFetch';
 import './App.css';
 
 const App = () => {
-  const { request } = useFetch();
-
-  // const myRequest = async (url, options) => {
-  //   const responseData = await request(url, options);
-  //   window.localStorage.getItem('wideToken', data.access_token);
-  //   window.localStorage.getItem('wideRefreshToken', data.refresh_token);
-  // };
+  const { data, error, loading, request } = useFetch();
+  const [userId, setUserId] = useState(null);
+  const [playlists, setPlaylists] = useState(null);
 
   useEffect(() => {
     let params = new URLSearchParams(window.location.search);
@@ -19,31 +22,105 @@ const App = () => {
     if (code && !window.localStorage.getItem('wideRefreshToken')) {
       const { url, options } = REQUEST_TOKEN(code);
 
-      async function myRequest(url, options) {
+      async function requestToken(url, options) {
         const { response, json } = await request(url, options);
         if (response.ok) {
           window.localStorage.setItem('wideToken', json.access_token);
           window.localStorage.setItem('wideRefreshToken', json.refresh_token);
         }
       }
-      myRequest(url, options);
+      requestToken(url, options);
     } else {
-      console.log(
-        'refresh token',
-        window.localStorage.getItem('wideRefreshToken'),
-      );
       console.log('token', window.localStorage.getItem('wideToken'));
+      async function requestUserProfile() {
+        const { url, options } = REQUEST_USER_PROFILE(
+          window.localStorage.getItem('wideToken'),
+        );
+        const { response, json } = await request(url, options);
+        if (response.ok) {
+          setUserId(json.id);
+          const { url, options } = REQUEST_PLAYLISTS(
+            window.localStorage.getItem('wideToken'),
+          );
+          const {
+            response: responsePlaylists,
+            json: jsonPlaylists,
+          } = await request(url, options);
+          if (responsePlaylists.ok) {
+            const myPlaylistWithMusics = await Promise.all(
+              jsonPlaylists.items.map(async (playlist) => {
+                const { url, options } = REQUEST_TRACKS_OF_PLAYLIST(
+                  window.localStorage.getItem('wideToken'),
+                  playlist.tracks.href,
+                );
+                const { response, json } = await request(url, options);
+                console.log('oi', response, json.items);
+                return {
+                  ...playlist,
+                  myMusics: json.items,
+                };
+              }),
+            );
+
+            setPlaylists(myPlaylistWithMusics);
+          }
+        } else {
+          const { url, options } = REFRESH_TOKEN(
+            window.localStorage.getItem('wideRefreshToken'),
+          );
+          const { response, json } = await request(url, options);
+          if (response.ok) {
+            const newToken = json.access_token;
+            window.localStorage.setItem('wideToken', newToken);
+            requestUserProfile();
+          }
+        }
+      }
+      requestUserProfile();
     }
   }, [request]);
 
-  const link_authorize = API_AUTHORIZE_SPOTIFY();
+  const link_authorize = REQUEST_CODE_AUTHORIZATION();
 
-  return (
-    <>
-      {window.localStorage.getItem('wideToken') && <p>Acessou</p>}
-      <a href={link_authorize}>Conectar</a>
-    </>
-  );
+  if (error) return <p>Erro: {error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <>
+        {userId && (
+          <>
+            <p>{userId}</p>
+            {/* <button onClick={handleSignOut}>Sair</button> */}
+          </>
+        )}
+        {window.localStorage.getItem('wideToken') ? (
+          <p>Acessou</p>
+        ) : (
+          <a href={link_authorize}>Conectar</a>
+        )}
+        {console.log('final', playlists)}
+        {playlists && (
+          <>
+            <h3>Playlists</h3>
+            {playlists.map((playlist) => (
+              <div key={playlist.id}>
+                <img
+                  src={playlist.images[2].url}
+                  alt={`playlist ${playlist.name}`}
+                />
+                <p>{playlist.name}</p>
+                <ul>
+                  {playlist.myMusics.map(({ track }) => (
+                    <li>{track.name}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </>
+        )}
+      </>
+    );
+  else return null;
 };
 
 export default App;
